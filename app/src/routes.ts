3a30a09bc9e70b4e05ceabd49e48a65a548d0067
vedanshiprajapati <vedanshi3012p@gmail.com>
2025-07-16 23:58:43 +0530

    :fix

--- app/src/routes.ts
+++ app/src/routes.ts
@@ -1,7 +1,7 @@
-export const publicRoutes = ["/", "/try"];
+export const publicRoutes = ["/chat", "/try"];
 export const DEFAULT_LANDING_PAGE = "/";
 export const authRoutes = ["/auth/signin", "/auth/signup", "/auth/error"];
-export const protectedRoutes = ["/chat", "/settings", "/try"];
+export const protectedRoutes = ["/settings", "/try"];
 export const apiAuthPrefix = "/api/auth";
 
 export const DEFAULT_LOGIN_REDIRECT = "/chat/home";
